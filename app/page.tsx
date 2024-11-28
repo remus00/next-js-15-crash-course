4323a14657bba0a62e0900fe75d569fb86ab4473
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-11-28 14:48:36 +0100

    client or server - component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Hello from '@/components/Hello';
+
 export default function Home() {
+    console.log('What am I? --SERVER/CLIENT');
+
     return (
-        <div className="flex h-screen w-full items-center justify-center">
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
             <h1 className="text-xl text-emerald-600">
                 Welcome to the Next.js 15 Crash course
             </h1>
+
+            <Hello />
         </div>
     );
 }
---
+++ components/Hello.tsx
@@ -0,0 +1,7 @@
+'use client';
+const Hello = () => {
+    console.log("I'm a client component");
+    return <div>Hello World - Client side</div>;
+};
+
+export default Hello;
