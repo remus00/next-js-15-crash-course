5d3eb47d819baeb313e7dab629d49da0084957e5
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-11-28 14:39:45 +0100

    Setup projects

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="flex h-screen w-full items-center justify-center">
+            <h1 className="text-xl text-emerald-600">
+                Welcome to the Next.js 15 Crash course
+            </h1>
+        </div>
+    );
+}
